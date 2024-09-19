6c0c24d4dca0e5955ccab0814e97466885596887
Sandra Pirpamer <spir611@g.educaand.es>
2024-09-19 21:29:16 +0200

    plantilla

---
+++ ejercicios/js/script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+const boton = document.getElementById("boton");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", function () {
+    mostrar(resultado);
+});
