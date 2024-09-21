543b214b223816c7dfd4692be9cf2698c332f608
Sandra Pirpamer <spir611@g.educaand.es>
2024-09-21 14:22:26 +0200

    Añadir cambias de estética a la página

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 "use strict"
 const nombreEjercicio = resultado.getAttribute('name');
 
-function mostrar() {
-    console.log(`Estoy mostrando el resultado del ${nombreEjercicio}`);
-    resultado.innerHTML = `Estoy mostrando el resultado del ${nombreEjercicio}`;
+function mostrar(resultado, texto) {
+    console.log(texto);
+    resultado.innerHTML = texto;
 }
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 "use strict"
-
-const boton = document.getElementById("boton");
 const resultado = document.getElementById("resultado");
+const texto=`Estoy mostrando el resultado del ${resultado.getAttribute('name')}`
+const boton = document.getElementById("boton");
 
 boton.addEventListener("click", function () {
-    mostrar();
+    mostrar(resultado, texto);
 });
